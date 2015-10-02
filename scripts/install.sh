9f3a20cf45d7c861f2c585cd25acdbddbc97b5f0
Andrzej Kopeć <kapkowski@gmail.com>
2015-10-03 00:00:12 +0200

    Prepared scripts for easier setup and run

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npm install -g http-server typescript
+tsd install
+jspm install
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tmux gem ruby ruby-dev tmux
+sudo gem install tmuxinator
+
+mkdir ~/.tmuxinator
+path=`pwd`
+sed "s,projectPath,$path,g" ./scripts/angular2-todo.yml > ~/.tmuxinator/angular2-todo.yml
