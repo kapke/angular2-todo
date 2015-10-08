c53a31d145267108fe44d3d8d85f01b02de4a0cd
Andrzej Kopeć <kapkowski@gmail.com>
2015-10-08 22:09:41 +0200

    Moved sorting into select

--- src/ts/components/TodosApp.ts
+++ src/ts/components/TodosApp.ts
@@ -1,65 +1,63 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
-import {Component, View, NgFor, NgClass} from 'angular2/angular2';
+import {Component, View, NgFor, NgClass, FORM_DIRECTIVES} from 'angular2/angular2';
 import TodoItem from './TodoItem';
 import Todo from '../Todo';
 import TodoRepository from '../TodoRepository';
 
+interface SortingOptionDescriptor {
+    title: string,
+    //0 - not sorting, 1 - increasing, -1 - decreasing
+    value:{property:string, direction: number}
+}
+
 @Component({
     selector: 'todos-app'
 }) @View({
-    directives: [NgFor, NgClass, TodoItem],
+    directives: [NgFor, NgClass, TodoItem, FORM_DIRECTIVES],
     styleUrls: ['src/style/todos-app.css'],
     templateUrl: 'src/template/todos-app.html'
 })
 class TodosApp {
-    todos:Array<Todo>;
-    sortingOptions = {
-        title: 0,
-        status: 0
-    };
+    public todos:Array<Todo>;
+    public sortingOptions:SortingOptionDescriptor[] = [];
 
     constructor (todoRepository:TodoRepository) {
+        ['title', 'status'].forEach((name) => {
+            this.sortingOptions.push(this.getSortingOptionDescriptor(name, 1));
+            this.sortingOptions.push(this.getSortingOptionDescriptor(name, -1));
+        });
         todoRepository.findTodos().then((todos) => {
             this.todos = todos;
         });
     }
 
-    public sortTodosBy (name:string) {
-        this.updateSortOptions(name);
-        this.sortTodos();
+    public sortTodos ($event) {
+        if($event.target.value) {
+            const [property, direction] = $event.target.value.split(':');
+            this.sortTodosBy(property, direction);
+        }
     }
 
-    private sortTodos () {
+    private sortTodosBy (name:string, direction:number) {
         this.todos = this.todos.sort((todo1:Todo, todo2:Todo) => {
-            if(this.sortingOptions.title) {
-                return todo1.compareByTitle(todo2)*this.sortingOptions.title;
-            }
-            if(this.sortingOptions.status) {
-                return todo1.compareByStatus(todo2)*this.sortingOptions.status;
+            if (name == 'title') {
+                return todo1.compareByTitle(todo2) * direction;
+            } else if (name == 'status') {
+                return todo1.compareByStatus(todo2) * direction;
             }
             return 0;
         });
     }
 
-    private updateSortOptions (name:string) {
-        //0 - not sorting, 1 - increasing, -1 - decreasing
-        const currentValue = this.sortingOptions[name];
-        let newValue = 0;
+    private getSortingOptionDescriptor (name, direction) {
+        const suffix = direction == 1 ? 'ascending' : 'descending';
 
-        switch(currentValue) {
-            case -1:
-                newValue = 0;
-                break;
-            case 0:
-                newValue = 1;
-                break;
-            case 1:
-                newValue = -1;
-                break;
+        return {
+            title: name + ' ' + suffix, value: {
+                property: name, direction: direction
+            }
         }
-
-        this.sortingOptions[name] = newValue;
     }
 }
 export default TodosApp;
